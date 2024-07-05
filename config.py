72a57b00353762fd52b2fb66459da70f91a83075
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-07-05 14:55:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = 7477914057:AAF36unurvS0FNK7QlpxqXds5wRtdCeckaI
-DB_URI = mongodb+srv://reacode:<password>@reamusic.x7hyx2h.mongodb.net/?retryWrites=true&w=majority&appName=reamusic
-DB_NAME = farming
+TOKEN = '7477914057:AAF36unurvS0FNK7QlpxqXds5wRtdCeckaI'
+DB_URI = 'mongodb+srv://reacode:<password>@reamusic.x7hyx2h.mongodb.net/?retryWrites=true&w=majority&appName=reamusic'
+DB_NAME = 'farming'
 CHANNEL_ID = -1002192963993
 OWNER_ID = 7494727691  # Replace with the owner's user ID
